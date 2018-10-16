99cb087369d9c550c3bf1c99dcd9f6a500c7280b
bbqchickenrobot <bbqchickenrobot@gmail.com>
2018-10-15 21:55:18 -0700

    * .gitignore: Initial commit
    * TitanSoft.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * TitanSoft.csproj:
    * LoginModel.cs:
    * appsettings.Development.json:
    * TitanSoft.Models.csproj:
    * launchSettings.json:
    * MemberController.cs:
    * CatalogController.cs:
    * RentalsController.cs:
    * TitanSoft.DataAccess.csproj:

---
+++ TitanSoft.Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TitanSoft.Models
+{
+    [JsonObject()]
+    public class LoginModel
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+}
---
+++ TitanSoft/Controllers/MemberController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TitanSoft.Models;
+
+namespace TitanSoft.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<object>> Get(string id)
+        {
+            return "value";
+        }
+
+
+        [HttpPost]
+        public async Task Login([FromBody] LoginModel value)
+        {
+        }
+
+
+        [HttpPut()]
+        public async Task Update([FromBody] object user)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(string id)
+        {
+        }
+    }
+}
