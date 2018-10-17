3b6fe1fe3ce7481ebdaa1bf606712ac958313706
bbqchickenrobot <bbqchickenrobot@gmail.com>
2018-10-17 10:29:39 -0700

    added initial models

--- TitanSoft.DataAccess/RavenDocumentStore.cs
+++ TitanSoft.DataAccess/RavenDocumentStore.cs
@@ -4,17 +4,17 @@ using Raven.Embedded;
 
 namespace TitanSoft.DataAccess
 {
-    public class DocumentStoreHolder
+    public class RavenDocumentStore
     {
-        private static Lazy<IDocumentStore> store = new Lazy<IDocumentStore>(CreateStore);
+        private static readonly Lazy<IDocumentStore> store = new Lazy<IDocumentStore>(CreateStore);
 
-        public static IDocumentStore Store => store.Value;
+        public static IDocumentStore Store { get => store.Value; }
 
         private static IDocumentStore CreateStore()
         {
             var serverOptions = new ServerOptions()
             {
-                ServerUrl = "http://127.0.0.1:60956/",
+                ServerUrl = "http://127.0.0.1:60956/"
             };
 
             EmbeddedServer.Instance.StartServer(serverOptions);
--- TitanSoft.Models/SearchModel.cs
+++ TitanSoft.Models/SearchModel.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
 namespace TitanSoft.Models
 {
     public class SearchModel
     {
-        public SearchModel()
-        {
-        }
+        [JsonProperty("Search")]
+        public List<Search> Search { get; set; }
+    }
+
+    public class Search
+    {
+        [JsonProperty("Title")]
+        public string Title { get; set; }
+
+        [JsonProperty("Year")]
+        public long Year { get; set; }
+
+        [JsonProperty("imdbID")]
+        public string ImdbId { get; set; }
+
+        [JsonProperty("Type")]
+        public string Type { get; set; }
+
+        [JsonProperty("Poster")]
+        public Uri Poster { get; set; }
     }
 }
