2a4d10ff9f443463331760add9cce6dba9451d32
bbqchickenrobot <bbqchickenrobot@gmail.com>
2018-10-18 16:30:40 -0700

    minor fixes/tweaks to various controllers, etc...

--- TitanSoft.Models/RentModel.cs
+++ TitanSoft.Models/RentModel.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace TitanSoft.Models
 {
-    public class RentModel
+    public class RentalModel
     {
-        public RentModel()
-        {
-        }
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+
+        [JsonProperty("movie_id")]
+        public string MovieId { get; set; }
+
+        [JsonProperty("date")]
+        public DateTime? DateRented { get; set; } = DateTime.Now;
+
+        [JsonProperty("price")]
+        public Decimal Price { get; set; }
+
+        [JsonProperty("expiring")]
+        public DateTime? Expiring { get => DateRented?.AddDays(14); }
     }
 }
